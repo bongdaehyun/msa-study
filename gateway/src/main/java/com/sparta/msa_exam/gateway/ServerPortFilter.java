a32847580c00708a22c9f0950cd3408f791a27fe
bongdaehyun <pkjeogus@gmail.com>
2024-08-06 21:24:01 +0900

    fix add port in header

--- gateway/src/main/java/com/sparta/msa_exam/gateway/ServerPortFilter.java
+++ gateway/src/main/java/com/sparta/msa_exam/gateway/ServerPortFilter.java
@@ -3,59 +3,56 @@ package com.sparta.msa_exam.gateway;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import lombok.val;
+import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
-import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.cloud.gateway.route.Route;
 import org.springframework.cloud.gateway.support.ServerWebExchangeUtils;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
+import java.util.List;
 
-import java.net.URI;
-
+/**
+ * gateway 단에서 response 헤더에 server port 추가
+ * 1. 요청이 들어오면 route를 이용하여 해당 서비스를 호출
+ * 2. 사용한 route의 service id를 추출
+ * 3. eureka instance의 정보들에서 server port추출
+ * 4. response header에 server port 추가
+ *
+ **/
 @RequiredArgsConstructor
 @Slf4j
+@Component
 public class ServerPortFilter implements GlobalFilter {
 
+    //eureka client들의 인스턴스 정보들에 접근 가능
     private final DiscoveryClient discoveryClient;
 
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
         return chain.filter(exchange).then(Mono.fromRunnable(() -> {
-            // Get the URI from the exchange attributes
-            URI requestUrl = (URI) exchange.getAttributes().get(ServerWebExchangeUtils.GATEWAY_REQUEST_URL_ATTR);
+            //ServerWebExchangeUtils : 주로 필터나 라우팅 관련 속성에 접근하는 메서드를 제공
+            Route route = exchange.getAttribute(ServerWebExchangeUtils.GATEWAY_ROUTE_ATTR);
+            if (route != null) {
+                String serviceId = route.getId();
+                log.info("serviceId : {}", serviceId);
 
-            if (requestUrl != null) {
-                // Get the service ID from the URI path
-                log.info("serviceId" + requestUrl.getHost());
-                String serviceId = requestUrl.getHost();
-                // Retrieve the port information from the DiscoveryClient
-                var intancse = discoveryClient.getInstances(serviceId);
-                if(intancse.isEmpty())
-                {
-                    log.warn("intance empty" + serviceId);
-                }else {
-
-
-                    String serverPort = discoveryClient.getInstances(serviceId)
-                            .stream()
-                            .findFirst()
-                            .map(instance -> instance.getMetadata().get("port"))
-                            .orElse("unknown");
-
-                    log.info("Server port: " + serverPort);
-                    log.info(discoveryClient.getInstances(serviceId).toString());
-                    // Add the port information to the response header
-                    exchange.getResponse().getHeaders().add("Service-Port", serverPort);
+                List<ServiceInstance> instances = discoveryClient.getInstances(serviceId);
+                if (!instances.isEmpty()) {
+                    ServiceInstance instance = instances.get(0);
+                    String port = String.valueOf(instance.getPort());
+                    log.info("Service: {}, Host: {}, Port: {}", serviceId, instance.getHost(), port);
+                    exchange.getResponse().getHeaders().add("Server-Port", port);
+                } else {
+                    log.warn("NOT FOUND INSTANCES {}", serviceId);
                 }
-//                discoveryClient.getInstances(serviceId).stream()
-//                        .findFirst()
-//                        .ifPresent(instance -> {
-//                            String port = String.valueOf(instance.getPort());
-//                            exchange.getResponse().getHeaders().add("Server-Port", port);
-//                        });
+            } else {
+                log.warn("NOT FOUND ROUTE IN GATEWAY");
             }
         }));
     }
+
+
 }
