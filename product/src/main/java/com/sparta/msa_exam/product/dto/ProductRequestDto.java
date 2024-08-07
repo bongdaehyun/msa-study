9cf49bea252130aab29e7ce9cb9e42d1293331ab
bongdaehyun <pkjeogus@gmail.com>
2024-08-07 11:27:51 +0900

    add Service test code

--- product/src/main/java/com/sparta/msa_exam/product/dto/ProductRequestDto.java
+++ product/src/main/java/com/sparta/msa_exam/product/dto/ProductRequestDto.java
@@ -1,8 +1,10 @@
 package com.sparta.msa_exam.product.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class ProductRequestDto {
     private String name;
 
---
+++ product/src/test/java/com/sparta/msa_exam/product/ProductServiceTest.java
@@ -0,0 +1,88 @@
+package com.sparta.msa_exam.product;
+
+import com.sparta.msa_exam.product.dto.Product;
+import com.sparta.msa_exam.product.dto.ProductRequestDto;
+import com.sparta.msa_exam.product.dto.ProductResponseDto;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+
+@ExtendWith(MockitoExtension.class)
+class ProductServiceTest {
+
+    @Mock
+    private ProductRepository productRepository;
+
+    private ProductService productService;
+
+    @BeforeEach
+    void setUp() {
+        productService = new ProductService(productRepository);
+    }
+
+
+    @Test
+    @DisplayName("상품 목록 조회 테스트")
+    void test1(){
+        //given
+        Product product1 = new Product(1L, "test1",1000);
+        Product product2 = new Product(2L, "test2",2000);
+
+        List<Product> productList = new ArrayList<>();
+        productList.add(product1);
+        productList.add(product2);
+
+        given(productRepository.findAll()).willReturn(productList);
+
+        //when
+        List<ProductResponseDto> lists = productService.getAllProducts();
+
+        //then
+        assertNotNull(lists);
+        assertEquals(lists.size(), 2);
+        assertEquals(lists.get(0).getName(), product1.getName());
+    }
+
+    @Test
+    @DisplayName("상품 생성 실패")
+    void test2(){
+        ProductRequestDto responseDto = new ProductRequestDto("test",1000);
+
+        given(productRepository.save(any(Product.class))).willReturn(null);
+
+        //when
+        Exception thrown = assertThrows(RuntimeException.class, () -> {
+            productService.createProduct(responseDto);
+        });
+
+        assertEquals(thrown.getMessage(), "Product not created");
+
+    }
+
+    @Test
+    @DisplayName("상품 생성 성공")
+    void test3(){
+        ProductRequestDto responseDto = new ProductRequestDto("test",1000);
+        Product product = Product.builder()
+                .name(responseDto.getName())
+                .supply_price(responseDto.getSupply_price())
+                .build();
+
+        given(productRepository.save(any(Product.class))).willReturn(product);
+
+        //when
+        assertDoesNotThrow( () -> productService.createProduct(responseDto));
+
+
+    }
+}
