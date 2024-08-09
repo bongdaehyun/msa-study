a275b380b9506d541ce848ffc3318c212577fb53
bongdaehyun <pkjeogus@gmail.com>
2024-08-09 11:22:10 +0900

    fix the config files

--- order/src/main/java/com/sparta/msa_exam/order/orders/OrderResponseDto.java
+++ order/src/main/java/com/sparta/msa_exam/order/orders/OrderResponseDto.java
@@ -3,12 +3,15 @@ package com.sparta.msa_exam.order.orders;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import java.io.Serializable;
 import java.util.List;
 
 @Data
 @AllArgsConstructor
-public class OrderResponseDto {
+@NoArgsConstructor
+public class OrderResponseDto implements Serializable {
     private Long order_id;
     private List<OrderItemDto> product_ids;
 
--- order/src/main/java/com/sparta/msa_exam/order/orders/OrderService.java
+++ order/src/main/java/com/sparta/msa_exam/order/orders/OrderService.java
@@ -3,12 +3,16 @@ package com.sparta.msa_exam.order.orders;
 import com.sparta.msa_exam.order.client.ProductClient;
 import com.sparta.msa_exam.order.client.ProductResponseDto;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 public class OrderService {
+    private static final Logger log = LoggerFactory.getLogger(OrderService.class);
     private final OrderRepository orderRepository;
     private final ProductClient productClient;
 
@@ -27,9 +31,10 @@ public class OrderService {
         orderRepository.save(newOrder);
         return newOrder;
     }
-
+    @Cacheable(value = "orderCache", key = "#orderId")
     @Transactional(readOnly = true)
     public OrderResponseDto getOrderDetail(long orderId) {
+        log.info("getOrderDetail 실행~~~");
         //해당 order 존재 검증
         Order order = orderRepository.findOrderAndItemById(orderId);
         if(order == null) {
