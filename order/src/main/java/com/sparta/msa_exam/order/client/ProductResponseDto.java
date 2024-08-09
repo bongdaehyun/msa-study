744ff740a50f4139edde87b75d846978a793ff49
bongdaehyun <pkjeogus@gmail.com>
2024-08-09 16:00:29 +0900

    update readme and testcode

--- order/src/main/java/com/sparta/msa_exam/order/client/ProductResponseDto.java
+++ order/src/main/java/com/sparta/msa_exam/order/client/ProductResponseDto.java
@@ -1,8 +1,12 @@
 package com.sparta.msa_exam.order.client;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class ProductResponseDto {
     private Long product_id;
     private String name;
--- order/src/test/java/com/sparta/msa_exam/order/orders/OrderServiceTest.java
+++ order/src/test/java/com/sparta/msa_exam/order/orders/OrderServiceTest.java
@@ -10,10 +10,13 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.verify;
 
 @ExtendWith(MockitoExtension.class)
 class OrderServiceTest {
@@ -89,4 +92,54 @@ class OrderServiceTest {
         assertEquals(exception.getMessage(),"잘못된 상품번호 입니다.");
     }
 
+    @Test
+    @DisplayName("존재하지 않는 주문번호로 주문에 상품을 추가하는 경우")
+    void testUpdateOrderUnsuccessfulByNotExistingOrder(){
+        Order order = new Order(1L,"test",new ArrayList<>());
+        given(orderRepository.findOrderAndItemById(1L)).willReturn(null);
+
+        Exception exception = assertThrows(IllegalArgumentException.class, ()
+        -> orderService.updateOrderItem(order.getOrderId(),new AddItemRequestDto())
+
+        );
+
+        assertEquals(exception.getMessage(),"잘못된 주문 ID입니다.");
+    }
+
+    @Test
+    @DisplayName(" 주문에 존재하지 앓는 상품을 추가하는 경우")
+    void testUpdateOrderUnSuccessfullByNotExistingOrderItem(){
+        Order order = new Order(1L,"test",new ArrayList<>());
+
+        given(orderRepository.findOrderAndItemById(1L)).willReturn(order);
+
+        List<ProductResponseDto> productResponseDtoList = new ArrayList<>();
+        productResponseDtoList.add(new ProductResponseDto(1L,"test",1));
+
+        given(productClient.getAllProducts()).willReturn(productResponseDtoList);
+
+        Exception exception = assertThrows(IllegalArgumentException.class, ()
+                -> orderService.updateOrderItem(order.getOrderId(),new AddItemRequestDto(3L))
+
+        );
+
+        assertEquals(exception.getMessage(),"존재하지 않는 상품입니다.");
+    }
+
+    @Test
+    @DisplayName("주문에 상품을 성공적으로 추가하는 경우")
+    void testUpdateOrderSuccessfullByExistingOrderItem(){
+        Order order = new Order(1L,"test",new ArrayList<>());
+
+        given(orderRepository.findOrderAndItemById(1L)).willReturn(order);
+
+        List<ProductResponseDto> productResponseDtoList = new ArrayList<>();
+        productResponseDtoList.add(new ProductResponseDto(1L,"test",1));
+        productResponseDtoList.add(new ProductResponseDto(2L,"test",2));
+
+        given(productClient.getAllProducts()).willReturn(productResponseDtoList);
+
+        orderService.updateOrderItem(order.getOrderId(),new AddItemRequestDto(1L));
+
+    }
 }
